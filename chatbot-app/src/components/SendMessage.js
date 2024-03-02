2d776caad1632eab91e980702c5944019fee5db6
Kasidit Bualoeng <subdit@gmail.com>
2024-03-01 23:24:31 -0500

    edited sendMessage component

--- chatbot-app/src/components/SendMessage.js
+++ chatbot-app/src/components/SendMessage.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth, db } from '../firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
-const SendMessage = () => {
-  return <div>Send Message here!!</div>;
+const SendMessage = async event => {
+  const [message, setMessage] = useState('');
+  event.preventDefault();
+  if (message.trim() === '') {
+    alert('Enter valid message');
+    return;
+  }
+  const { uid, displayName, photoURL } = auth.currentUser;
+  await addDoc(collection(db, 'messages'), {
+    text: message,
+    name: displayName,
+    avatar: photoURL,
+    createAt: serverTimestamp(),
+    uid
+  });
+  setMessage();
+
+  return (
+    <form onSubmit={event => SendMessage(event)} className='send-message'>
+      <label htmlFor='messageInput' hidden>
+        Enter Message
+      </label>
+      <input
+        id='messageInput'
+        name='messageInput'
+        type='text'
+        className='form-input__input'
+        placeholder='type message here'
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type='submit'>Send</button>
+    </form>
+  );
 };
 
 export default SendMessage;
