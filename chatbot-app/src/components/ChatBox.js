1b4eace2d3188e068542129cae7f646e28ea9bdc
Kasidit Bualoeng <subdit@gmail.com>
2024-03-02 14:42:54 -0500

    edited and modified ChatBox component and add useEffect useRef function to handle the messages

--- chatbot-app/src/components/ChatBox.js
+++ chatbot-app/src/components/ChatBox.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit
+} from 'firebase/firestore';
+import { db } from '../firebase';
+import SendMessage from './SendMessage';
 
 const ChatBox = () => {
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, 'message'),
+      orderBy('createAt', 'desc'),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, QuerySnapshot => {
+      const fetchedMessages = [];
+      QuerySnapshot.forEach(doc => {
+        fetchedMessages.push({ ...doc.data(), id: doc.id });
+      });
+      const sortedMessages = fetchedMessages.sort(
+        (a, b) => a.createdAt - b.createdAt
+      );
+      setMessages(sortedMessages);
+    });
+    return () => unsubscribe;
+  });
   return (
-    <div>
-      <h1>Chat Box ..</h1>
-    </div>
+    <main className='chat-box'>
+      <div className='messages-wrapper'>
+        {messages?.map(message => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
+      <span ref={scroll}></span>
+      <SendMessage scroll={scroll} />
+    </main>
   );
 };
 
